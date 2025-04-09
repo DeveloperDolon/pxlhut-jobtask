2724865eb0e4f46f1ab2fb2c4f0b58b4315797f7
DeveloperDolon <dolonr718@gmail.com>
2025-04-09 21:03:19 +0600

    form field validation and design added

--- app/components/FormField.tsx
+++ app/components/FormField.tsx
@@ -1,24 +1,25 @@
 import { FormFieldProps } from "../types/formTypes";
 
-
 const FormField: React.FC<FormFieldProps> = ({
     type,
     placeholder,
     name, 
     register,
     error,
-    valueAsNumber
+    valueAsNumber,
+    className
 }) => {
     return (
         <>
             <input
+                className={`input-field block md:px-3 md:py-2 rounded-lg dark:bg-white bg-[#e8f0fe] md:placeholder:text-sm placeholder:text-xs  ${className}`}
                 type={type}
                 placeholder={placeholder}
                 {...register(name, { valueAsNumber })}
             />
-            {error && <span className="error-message">{error.message}</span>}
+            {error && <> <span className="error-message text-red-500 md:text-xs text-[10px]">{error.message}</span></>}
         </>
     );
 };
 
-export default FormField;   
+export default FormField;
--- app/types/formTypes.ts
+++ app/types/formTypes.ts
@@ -19,6 +19,7 @@ export type FormFieldProps = {
   register: UseFormRegister<FromData>;
   error: FieldError | undefined;
   valueAsNumber?: boolean;
+  className?: string;
 };
 
 export type ValidFieldNames =
