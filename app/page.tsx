80c2aeecc91f1b8a3b8ed04475601ad8169d0c90
DeveloperDolon <dolonr718@gmail.com>
2025-04-09 21:43:49 +0600

    some design added

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
   const mode = useAppSelector((state) => state.mode.mode);
   const dispatch = useAppDispatch();
   return (
-    <div className="font-[family-name:var(--font-geist-sans)]">
-      <div className="flex justify-center flex-col md:mt-5 mt-3.5">
+    <div className="font-[family-name:var(--font-geist-sans)] mb-20">
+      <div className="flex justify-center flex-col mt-10">
         <button
           onClick={() => dispatch(setMode(mode == "dark" ? "light" : "dark"))}
           className="cursor-pointer px-2 py-1 text-white dark:text-gray-600 dark:bg-white bg-gray-500 w-fit mx-auto rounded-lg"
@@ -21,7 +21,6 @@ export default function Home() {
           Multi-Step Form with Validation
         </h2>
       </div>
-
       <Form/>
     </div>
   );
