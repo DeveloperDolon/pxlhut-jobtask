e2f77b89cb544fb5046d8b073354b3cdb298b122
DeveloperDolon <dolonr718@gmail.com>
2025-04-09 13:10:08 +0600

    some configuration added

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="font-[family-name:var(--font-geist-sans)]">
+    <div className="font-[family-name:var(--font-geist-sans)] dark:bg-gray-500">
       Hello world from bangladeh.
     </div>
   );
