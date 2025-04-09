c175289aaf1c6e2e040fdfc6ca777c200747e998
DeveloperDolon <dolonr718@gmail.com>
2025-04-09 21:47:14 +0600

    mode button text refactor

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
           onClick={() => dispatch(setMode(mode == "dark" ? "light" : "dark"))}
           className="cursor-pointer px-2 py-1 text-white dark:text-gray-600 dark:bg-white bg-gray-500 w-fit mx-auto rounded-lg"
         >
-          {mode?.toUpperCase()}
+          {mode == "dark" ? "Light" : "Dark"}
         </button>
         <h2 className="md:text-4xl sm:text-2xl text-xl font-semibold text-center dark:text-gray-100">
           Multi-Step Form with Validation
